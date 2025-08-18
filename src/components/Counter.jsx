1f2a017a10dfcc9e6a05504da27ea55520453e8f
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-18 23:13:16 +0530

    Counter method implemented

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,24 @@
 const Counter = () => {
 
+    let counter = 0;
+
+    const increase = () =>{
+        counter++;
+        console.log("Counter ", counter)
+    }
+
+    const decrease = () =>{
+        counter--;
+        console.log("cpunter ", counter)
+    }
+
 
     return (
 
         <div>
             <h3>Counter</h3>
+            <button onClick={increase}>Increase</button>
+            <button onClick={decrease}>Decrease</button>
         </div>
     )
 }
