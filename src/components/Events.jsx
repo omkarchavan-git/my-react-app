c57f20489c7ab6879e001f1c2ae2e072b2d8b499
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-18 22:58:16 +0530

    call back function implementation

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,17 +1,32 @@
 import react from 'react'
 
-const Events= () => {
-    const alertButton =() => {
+const Events = () => {
+    const alertButton = () => {
         alert("Hello")
     }
-    return(
+
+
+    // call back function   ---> this function allow us to pass the argument first and then call the function
+
+    const callBack = (a) => {
+        alert(a + 10)
+    }
+
+    return (
         <div>
             <h1>use state </h1>
+
+            {/* calling normal function */}
             <button onClick={alertButton}> click </button>
 
+            {/* calling call back function */}
+            <button onClick={() => callBack(10)}> call back function </button>
 
         </div>
     )
 }
+
+
+
 
 export default Events
