e54ef1909b703332cfac882aaeca2c6735e3fbc8
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-22 18:34:15 +0530

    updated

--- src/pages/Course_details.jsx
+++ src/pages/Course_details.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Course_details = () => {
+
+      const arr = [
+        { id: 1, course_name: "java", price: 1234 },
+        { id: 2, course_name: "Python", price: 1435 },
+        { id: 3, course_name: "MERN", price: 12004 },
+        { id: 4, course_name: "AI-ML", price: 12344 },
+        { id: 5, course_name: "HTML", price: 9999 }
+
+    ]
+
+    const {id} = useParams()
+
+    const course_details = arr.filter( (data) => data.id == id)
+    console.log(course_details)
 
     return(
         <>
             <div>
 
-                <h3>Course details</h3>
+                <h3>Course id : {id} </h3>
             </div>
         </>
     )
