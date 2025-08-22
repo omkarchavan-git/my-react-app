8d2433cf78d33079f4cca98c836a2167cfcad5f2
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-22 18:58:16 +0530

    implemented use location

--- src/pages/Course_details.jsx
+++ src/pages/Course_details.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, useLocation } from 'react-router-dom'
 
 const Course_details = () => {
 
-      const arr = [
+    const arr = [
         { id: 1, course_name: "java", price: 12099 },
         { id: 2, course_name: "Python", price: 1499 },
         { id: 3, course_name: "MERN", price: 12999 },
@@ -12,18 +12,24 @@ const Course_details = () => {
 
     ]
 
-    const {id} = useParams()
+    const { id } = useParams()
 
-    const course_details = arr.filter( (data) => data.id == id)
+    const course_details = arr.filter((data) => data.id == id)
     console.log(course_details)
 
-    return(
+    const location = useLocation();
+
+    return (
         <>
             <div>
 
                 <h3>Course id : {id} </h3>
                 <h3>Course name : {course_details[0].course_name} </h3>
-                <h3>Course price : {course_details[0].price} </h3>
+                {location.pathname != "/cources/1" && (
+                    <>
+                        <h3>Course price : {course_details[0].price} </h3>
+                    </>
+                )}
             </div>
             <button type="button"> <Link to={'/cources'}>All Cources</Link> </button>
         </>
